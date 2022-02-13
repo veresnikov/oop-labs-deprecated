425d6642f70ae6a89c16d0a56216bad6cf069b67
Kirill Veresnikov <kirill.veres@list.ru>
2022-02-13 22:15:10 +0300

    Сделал задание 1, replace. Покрыл тестами

---
+++ lab1/replace/replace/replace.cpp
@@ -0,0 +1,98 @@
+﻿#include <fstream>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+const int ExpectedArgument = 5;
+const int ErrorStatusCode = 1;
+
+struct CommandLineArgument
+{
+	string InputFile;
+	string OutputFile;
+	string Search;
+	string Replace;
+};
+
+CommandLineArgument ParseCommandLineArgument(int argc, char* argv[])
+{
+	if (argc != ExpectedArgument)
+	{
+		cerr << "Invalid argument." << endl
+			 << "Usage: replace.exe <input file> <output file> <search string> <replace string>" << endl;
+		exit(ErrorStatusCode);
+	}
+	CommandLineArgument argument;
+	argument.InputFile = argv[1];
+	argument.OutputFile = argv[2];
+	argument.Search = argv[3];
+	argument.Replace = argv[4];
+
+	return argument;
+}
+
+string ReplaceString(const string& subject, const string& search, const string& replace)
+{
+	if (search.empty())
+	{
+		return subject;
+	}
+	string result = subject;
+	size_t position = 0;
+	while (position < result.length())
+	{
+		size_t replacedPosition = result.find(search, position);
+		if (replacedPosition == string::npos)
+		{
+			return result;
+		}
+		position = replacedPosition;
+
+		result.erase(replacedPosition, search.length());
+		result.insert(replacedPosition, replace);
+
+		position += replace.length();
+	}
+
+	return result;
+}
+
+void CopyFileWithReplace(istream& inputFile, ostream& outputFile, string search, string replace)
+{
+	string line;
+	while (!inputFile.eof())
+	{
+		getline(inputFile, line);
+		line = ReplaceString(line, search, replace);
+		outputFile << line;
+
+		if (!inputFile.eof())
+		{
+			outputFile << endl;
+		}
+	}
+
+	outputFile.flush();
+}
+
+int main(int argc, char* argv[])
+{
+	CommandLineArgument argument = ParseCommandLineArgument(argc, argv);
+
+	ifstream inputFile(argument.InputFile);
+	if (!inputFile.is_open())
+	{
+		cerr << "Input file does not exist." << endl;
+		exit(ErrorStatusCode);
+	}
+
+	ofstream outputFile(argument.OutputFile);
+	if (!outputFile.is_open())
+	{
+		cerr << "Error opening output file." << endl;
+		exit(ErrorStatusCode);
+	}
+
+	CopyFileWithReplace(inputFile, outputFile, argument.Search, argument.Replace);
+}
---
+++ lab1/replace/replace/test.bat
@@ -0,0 +1,57 @@
+@echo off
+
+set PROGRAM="%~1"
+if %PROGRAM%=="" (
+	echo Please specify path to the program
+	exit /B 1
+)
+
+set TEMPRESULT="%TEMP%\result.txt"
+set TEMPOUTPUT="%TEMP%\output.txt"
+
+echo %CD%
+
+echo Test invalid argument
+%PROGRAM% > %TEMPOUTPUT% 2>&1
+fc %TEMPOUTPUT% %CD%\test-data\TestInvalidArgument-Output.txt || goto :err
+
+echo Test invalid input filename
+%PROGRAM% "" %TEMPRESULT% test test > %TEMPOUTPUT% 2>&1
+fc %TEMPOUTPUT% %CD%\test-data\TestInvalidInputFilename-Output.txt || goto :err
+
+echo Test invalid output filename
+%PROGRAM% %CD%\test-data\TestInvalidOutputFilename-Input.txt "" test test > %TEMPOUTPUT% 2>&1
+fc %TEMPOUTPUT% %CD%\test-data\TestInvalidOutputFilename-Output.txt || goto :err
+
+echo Test empty search string
+%PROGRAM% %CD%\test-data\TestEmptySearchString-Input.txt %TEMPRESULT% "" test > %TEMPOUTPUT% 2>&1
+fc %TEMPRESULT% %CD%\test-data\TestEmptySearchString-Output.txt || goto :err
+
+echo Test empty replace string
+%PROGRAM% %CD%\test-data\TestEmptyReplaceString-Input.txt %TEMPRESULT% " " "" > %TEMPOUTPUT% 2>&1
+fc %TEMPRESULT% %CD%\test-data\TestEmptyReplaceString-Output.txt || goto :err
+
+echo Test multiple occurrences
+%PROGRAM% %CD%\test-data\TestMultipleOccurrences-Input.txt %TEMPRESULT% ma mama > %TEMPOUTPUT% 2>&1
+fc %TEMPRESULT% %CD%\test-data\TestMultipleOccurrences-Output.txt || goto :err
+
+echo Test equal search and replace
+%PROGRAM% %CD%\test-data\TestEqualSearchAndReplace-Input.txt %TEMPRESULT% test test > %TEMPOUTPUT% 2>&1
+fc %TEMPRESULT% %CD%\test-data\TestEqualSearchAndReplace-Output.txt || goto :err
+
+echo Test phrase-to-phrase replacement
+%PROGRAM% %CD%\test-data\TestPhraseToPhraseReplacement-Input.txt %TEMPRESULT% "test test" "42 42" > %TEMPOUTPUT% 2>&1
+fc %TEMPRESULT% %CD%\test-data\TestPhraseToPhraseReplacement-Output.txt || goto :err
+
+echo Test stress
+echo Time start: %time%
+%PROGRAM% %CD%\test-data\TestStress-Input.txt %TEMPRESULT% a A > %TEMPOUTPUT% 2>&1
+echo Time stop: %time%
+fc %TEMPRESULT% %CD%\test-data\TestStress-Output.txt || goto :err
+
+echo All test passed
+exit /B 0
+
+:err
+echo Test failed
+exit /B 1
